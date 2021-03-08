e96284c2338247834c9ad7c087a49021a4ba0daf
Michael Sissons <sissonsm@sheridancollege.ca>
2021-03-08 15:19:26 -0500

    Set up and test sprite mesh

---
+++ unity team 28/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    bool dragging;
+    public bool Dragging
+    {
+        get { return dragging; }
+    }
+    Rigidbody2D rigid;
+    public float dragSpeed = 5f;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            Vector2 mouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            rigid.velocity = mouseDir * dragSpeed;
+        }
+
+
+    }
+
+
+}
---
+++ unity team 28/Assets/Scripts/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTo(food.position);
+    }
+
+
+    void MoveTo(Vector2 point)
+    {
+        float move;
+        if (transform.position.x > point.x) move = -1f; else move = 1f;
+        if (Vector2.Distance(point, transform.position) < 1.1f) move = 0f;
+
+        transform.position = new Vector3(transform.position.x + move * Time.deltaTime * speed, transform.position.y);
+    }
+}
