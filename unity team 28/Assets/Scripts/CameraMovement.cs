efaa8d9ce2dbd0c414b221abaff4929f9e31258e
Samson Close <defaultsamson@users.noreply.github.com>
2021-03-09 16:49:12 -0500

    Mouse movement complete.

--- unity team 28/Assets/Scripts/CameraMovement.cs
+++ unity team 28/Assets/Scripts/CameraMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
 
-    //public Transform target;
+    private float threshold = 50F;
     public float smoothTime = 0.3F;
+    public float speed = 3F;
     private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
@@ -19,7 +20,31 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPos = transform.TransformPoint(new Vector3(1, 1, 0));
+        Vector3 movementVec = Vector3.zero;
+
+        // Right
+        if (Input.mousePosition.x >= Screen.width - threshold)
+        {
+            movementVec += new Vector3(speed, 0, 0);
+        }
+        // Left
+        else if (Input.mousePosition.x <= threshold)
+        {
+            movementVec += new Vector3(-speed, 0, 0);
+        }
+        
+        // Up
+        if (Input.mousePosition.y <= threshold)
+        {
+            movementVec += new Vector3(0, -speed, 0);
+        }
+        // Down
+        else if (Input.mousePosition.y >= Screen.height - threshold)
+        {
+            movementVec += new Vector3(0, speed, 0);
+        }
+
+        Vector3 targetPos = transform.TransformPoint(movementVec);
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
     }
 }
