ce466a921369e35555b9589f31b6bf23ad91efd1
Samson Close <defaultsamson@users.noreply.github.com>
2021-03-11 16:20:33 -0500

    Fixed error in random sound selection

--- unity team 28/Assets/Scripts/DragDrop.cs
+++ unity team 28/Assets/Scripts/DragDrop.cs
@@ -122,7 +122,7 @@ public class DragDrop : MonoBehaviour
                 // Play the sound at different volumes depending on the impact speed
                 float volume = Mathf.Clamp(Mathf.Abs(shadowObject.ElevationVelocity / 7F), 0.0F, 0.4F);
                 // Select a random landing audio
-                int rand = Random.Range(0, landingAudios.Length);
+                int rand = Random.Range(0, landingAudios.Length - 1);
                 audioSource.PlayOneShot(landingAudios[rand], volume);
                 landed = true;
             }
